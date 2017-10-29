2eb7681eef8d1b3a9434ecd5e07599c126b5a4f4
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-10-29 15:59:34 -0700

    CowTimer

---
+++ src/CowTimer.java
@@ -0,0 +1,60 @@
+import java.applet.AudioClip;
+import java.io.IOException;
+import javax.swing.JApplet;
+import javax.swing.Timer;
+
+public class CowTimer {
+	/*
+	 * This is an advanced recipe. There may be more than one line of code for
+	 * each instruction.
+	 * Work in seconds when testing, then change to minutes
+	 */
+	static CowTimer timer;
+	private int minutes;
+	public static void main(String[] args) throws InterruptedException {
+		/* 1. Make a CowTimer, set the time and start it. */
+		timer = new CowTimer();
+		timer.setTime(1);
+		timer.start();
+	}
+	public void setTime(int minutes) {
+		this.minutes = minutes;
+		System.out.println("Cow set to " + minutes + " minutes.");
+	}
+
+	public void start() throws InterruptedException {
+		/*
+		 * 2. Count down the minutes, print the current minute then sleep for 60
+		 * seconds using Thread.sleep(int milliseconds).
+		 */
+		for (int i = timer.minutes * 60; i > 0; i--) {
+			System.out.println(i);
+			Thread.sleep(1000);
+			}
+		/*
+		 * 3. When the timer is finished, use the playSound method to play a moo
+		 * sound. You can download one from freesound.org, then drag it into
+		 * your default package. Tell the students (by speaking) it's time to walk.
+		 */
+		if (timer.minutes == 0) {
+			//playSound();
+		}
+	}
+
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet
+				.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
+
+private void speak(String stuffToSay) {
+		try {
+			Runtime.getRuntime().exec("say " + stuffToSay).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+}
+
