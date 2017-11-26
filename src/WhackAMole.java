96990ca88f04ee0e8303bfb2e22a9a9be18e9f41
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-11-26 15:58:38 -0800

    whackamole

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,30 +1,44 @@
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
+	static Date startTime;
+	int wack = 0;
+	int empty = 0;
 	public static void main(String[] args) {
+		startTime = new Date();
 		Random random = new Random();
 		WhackAMole m = new WhackAMole();
 		int rr = random.nextInt(24);
 		m.drawButtons(rr);
 	}
-	static JButton button;
+	JButton button;
+	JButton mole;
+	JPanel panel;
+	JFrame frame; 
 	void drawButtons(int r) {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		
+		frame = new JFrame();
+		panel = new JPanel();
+		mole = new JButton("mole!");
 		for (int i = 0; i < 24; i++) {
 			button = new JButton();
 			if (i == r) {
-				button.setText("mole!");
+				panel.add(mole);
+				mole.addActionListener(this);
+			}
+			else {
+				panel.add(button);	
 				button.addActionListener(this);
 			}
-			panel.add(button);
 		}
 		frame.setVisible(true);
 		frame.setSize(300, 300);
@@ -37,10 +51,48 @@ public class WhackAMole implements ActionListener {
 			e.printStackTrace();
 			}
 		}
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+		+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+		}
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int r = rand.nextInt(24);
+		if (e.getSource() == mole) {
+			//playSound();
+			frame.dispose();
+			drawButtons(r);
+			wack = wack + 1;
+			if (wack == 10) {
+				endGame(startTime, 10);
+			}
+		}
 		
+		else if (e.getSource() != mole) {
+			empty = empty + 1;
+			if (empty < 2) {
+				speak("dork");
+			}
+			else if (empty ==  2) {
+				speak("idiot");
+			}
+			else if (empty ==  3) {
+				speak("moron");
+			}
+			else if (empty == 4) {
+				speak("Your a waste of atoms");
+			}
+			else {
+				speak("dork");
+			}
+		}
 	}
 
 }
